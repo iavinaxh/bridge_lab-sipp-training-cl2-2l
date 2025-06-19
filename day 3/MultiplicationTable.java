47bf2840e6033926c401026814a1df3084b2a519
Ayushchaubey-291102 <ayush.chaubey_cs22@gla.ac.in>
2025-06-19 09:59:22 +0530

    .

---
+++ day 3/MultiplicationTable.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        int[] results = new int[10];
+        
+        for (int i = 1; i <= 10; i++) {
+            results[i - 1] = number * i;
+        }
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " * " + i + " = " + results[i - 1]);
+        }
+        
+        scanner.close();
+    }
+}
